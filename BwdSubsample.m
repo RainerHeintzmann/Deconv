f26c7cdcde2de6e12bdd5c48dbfd2f7d8c51d4ca
Rainer Heintzmann <heintzmann@gmail.com>
2017-11-23 14:07:27 +0100

    Bug in BwdSubsample.m.

--- BwdSubsample.m
+++ BwdSubsample.m
@@ -2,4 +2,6 @@ function SubSampled=BwdSubsample(Recons,subSampling)
 if any(subSampling~=1)
     SubSampled=newim(size(Recons).*subSampling);
     SubSampled(0:subSampling(1):end,0:subSampling(2):end,0:subSampling(3):end)=Recons;
+else
+    SubSampled=Recons;
 end
